9ee96a1490b907740d5adf33a201b900647cfc27
SAHUL KUMAR <sahulshaw92@gmail.com>
2026-01-30 11:44:08 +0530

    index.html app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("hiii this is the first class");
+let students={name:"vikas",roll_no:953,sec:24};
+console.table(students);
+
+console.table([
+    {name:"sahul",roll_no:955,sec:24},
+    {name:"sahul",roll_no:955,sec:24},
+    {name:"sahul",roll_no:955,sec:24},
+    {name:"sahul",roll_no:955,sec:24},
+    {name:"sahul",roll_no:955,sec:24}
+    
+]);
+
+console.time("test");
+for(let i=0;i<10000;i++){}
+console.timeEnd("test");
