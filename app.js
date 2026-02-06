34e07619109aa8dae873f2053c40d2c8ec89abc6
SAHUL KUMAR <sahulshaw92@gmail.com>
2026-02-06 12:38:09 +0530

    06/02/LAB

--- app.js
+++ app.js
@@ -1,15 +1,95 @@
-let students={name:"vikas",roll_no:953,sec:24};
-console.table(students);
+// let students = { name: "vikas", roll_no: 953, sec: 24 };
+// console.table(students);
 
-console.table([
-    {name:"sahul",roll_no:955,sec:24},
-    {name:"sahul",roll_no:955,sec:24},
-    {name:"sahul",roll_no:955,sec:24},
-    {name:"sahul",roll_no:955,sec:24},
-    {name:"sahul",roll_no:955,sec:24}
-    
-]);
+// console.table([
+//     { name: "sahul", roll_no: 955, sec: 24 },
+//     { name: "sahul", roll_no: 955, sec: 24 },
+//     { name: "sahul", roll_no: 955, sec: 24 },
+//     { name: "sahul", roll_no: 955, sec: 24 },
+//     { name: "sahul", roll_no: 955, sec: 24 }
 
-console.time("test");
-for(let i=0;i<10000;i++){}
-console.timeEnd("test");
+// ]);
+
+// console.time("test");
+// for (let i = 0; i < 10000; i++) { }
+// console.timeEnd("test");
+
+// console.group("cse_24");
+// console.log("name:sahul");
+// console.groupEnd();
+
+// console.count("click");
+// console.count("click");
+// console.count("click");
+// console.count("click");
+// console.count("click");
+
+// console.dir(document.Document);
+// function add(p, q) {
+//     return p + q;
+// }
+// console.log(add(1, 2));
+
+// arrow function
+// function mul=(p,q)=>p*q;
+// console.log("arrow function",mul(5,5));
+
+//arrow
+// let arr=[10,20,30,40,50];
+// console.log(arr[0]);
+// arr.push(100);
+// arr.pop();
+// console.log(arr);
+
+//object
+// let student={
+//     name:"sahul",
+//     age:20,
+//     course:"b.tech"
+// };
+// console.log(student.name);
+
+// let today=new Date();
+// console.log(today);//gives time and date
+
+
+//MATH function
+// console.log(Math.pow(2,5));
+// console.log(Math.max(2,5));
+// console.log(Math.min(2,5));
+// console.log(Math.random(2,5));
+
+//STRING
+//=========//
+// text="hello world";
+// console.log(text.length);
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+// console.log(text.includes("java"));
+
+//TYPE CONVERSION
+//===============//
+// let strnum="12345";
+// let convert=Number(strnum);
+// console.log(convert+12);
+
+//NAN
+//===//
+// console.log(isNaN("abc"));
+// console.log(isNaN(123));
+
+// DOM MANUPULATION
+//==================//
+// setTimeout(()=>{
+//     console.log("executed after 2 time");
+// },20000);
+
+//SWITCH//
+//=========//
+// try{
+//     let sesult=x/0;
+//     console.log(result);
+// }
+// catch(error){
+//     console.log("error");
+// }
