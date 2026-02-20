71dca70943193af30fdc5e314acfd1c6d575a704
SAHUL KUMAR <sahulshaw92@gmail.com>
2026-02-20 11:42:18 +0530

    some micro projects

---
+++ calci/app.js
@@ -0,0 +1,6 @@
+function add(){
+    const n1=Number(document.getElementById("n1").value);
+    const n2=Number(document.getElementById("n2").value);
+    document.getElementById("result").innerHTML=n1+n2;
+
+}
---
+++ toggleButton/app.js
@@ -0,0 +1,8 @@
+function Toggletext(){
+let t=document.getElementById("text");
+if(t.style.display==="none"){
+    t.style.display="block";
+}else{
+    t.style.display="none";
+}
+}
